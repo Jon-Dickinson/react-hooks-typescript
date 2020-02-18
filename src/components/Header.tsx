4ee9c2d672e56ee2faca291a3a5621036ed4d5e0
ashleybrener <ashley@starlogik.com>
2020-02-19 00:35:52 +0200

    Dynamic styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Cssr } from './cssr';
+import styled from '@emotion/styled';
+import { cssrStyles, IStyleProps } from './cssr/styles';
+
+const H2 = styled.h2<IStyleProps>`
+  ${ cssrStyles }
+`;
 
 export default function Header() {
   return (
     <Cssr direction="vertical" background="651fff" justify="center" height="100p">
       <Cssr center="flex" maxWidth="768" flexDirection="column">
-        <h2 className="font-s--22 font-w--300 color--ffffff">
+        <H2 font={{ size: '22px', weight: 300 }} color="#ffffff">
           Hooks
-        </h2>
+        </H2>
       </Cssr>
     </Cssr>
   );
